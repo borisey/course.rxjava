6e49287e4910bd9bc3b366a4cf6295ff8fd0879a
borisey <51847223+borisey@users.noreply.github.com>
2025-04-16 20:49:26 +0300

    test testFilterOperator

--- src/test/java/ObservableTest.java
+++ src/test/java/ObservableTest.java
@@ -24,4 +24,21 @@ public class ObservableTest {
 
         assertEquals(Arrays.asList(2, 4, 6), result);
     }
+
+    @Test
+    public void testFilterOperator() {
+        Observable<Integer> observable = Observable.create(emitter -> {
+            emitter.onNext(1);
+            emitter.onNext(2);
+            emitter.onNext(3);
+            emitter.onComplete();
+        });
+
+        // применяем оператор filter
+        List<Integer> result = new ArrayList<>();
+        observable.filter(item -> item % 2 == 0)  // Оставляем только четные числа
+                .subscribe(result::add);
+
+        assertEquals(List.of(2), result);
+    }
 }
